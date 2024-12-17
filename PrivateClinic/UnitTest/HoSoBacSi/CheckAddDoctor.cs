a27127f29371feb3222101b4cafb953ebe2453fd
Vu Binh <22520129@gm.uit.edu.vn>
2024-12-17 09:48:18 +0700

    Nunit: Hoàn thành viết Nunit cho chức năng thêm bác sĩ

--- PrivateClinic/UnitTest/HoSoBacSi/CheckAddDoctor.cs
+++ PrivateClinic/UnitTest/HoSoBacSi/CheckAddDoctor.cs
@@ -5,6 +5,7 @@ using PrivateClinic.ViewModel.HoSoBacSiVM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -134,6 +135,66 @@ namespace PrivateClinic.UnitTest.HoSoBacSi
 			_viewModel.ValidateBangCap();
 			// Assert
 			Assert.That(_viewModel.BangCapError, Is.EqualTo(expectedError));
+		}
+
+		[TestCase("Trần Minh Hạ", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "", "", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "210", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "SĐT phải đủ 10 số", "", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "asd%10", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "SĐT chỉ chứa số", "", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "2876536987", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "SĐT phải bắt đầu bằng số 0", "", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "SĐT không được để trống!", "", "", "", "", "")]
+		[TestCase("Hạ #2 Minh", "", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "Họ và tên không chứa kí tự đặc biệt", "", "", "", "", "", "")]
+		[TestCase("", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "Họ và tên không được để trống!", "", "", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "123#", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "", "Email không hợp lệ!", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "", "Email không được để trống!", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", "2035-12-20", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "", "", "Ngày sinh không hợp lệ!", "Ngày vào làm phải lớn hơn ngày sinh!", "", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", null, "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "", "", "Ngày sinh không được để trống!", "", "", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "Tiến sĩ", "", "", "", "", "", "", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", null, "Đồng Nai", "Tiến sĩ", "", "", "", "", "Ngày vào làm không được để trống!", "", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "", "Tiến sĩ", "", "", "", "", "", "Địa chỉ không được để trống!", "")]
+		[TestCase("Trần Minh Hạ", "0873265725", "vubinh.2004.17.7@gmail.com", "Nam", "2008-04-24", "2013-03-20", "Đồng Nai", "", "", "", "", "", "", "", "Bằng cấp không được để trống!")]
+		public void CheckThemBacSi(string hoten, string sDT, string email, string sex, string birthday,
+										string startingday, string diachi, string bangcap,
+										string expectedFullNameError, string expectedSDTError,
+										string expectedEmailError, string expectedNgaySinhError,
+										string expectedNgayVLError, string expectedDiaChiError,
+										string expectedBangCapError)
+		{
+			//Arrange
+			_viewModel.HoTen = hoten;
+			_viewModel.SDT = sDT;
+			_viewModel.Email = email;
+			_viewModel.GioiTinh = sex;
+			DateTime? ngaysinh = null;
+			if (!string.IsNullOrWhiteSpace(birthday))
+			{
+				ngaysinh = DateTime.Parse(birthday);
+			}
+			_viewModel.NgaySinh = ngaysinh;
+			DateTime? ngayVaoLam = null;
+			if (!string.IsNullOrWhiteSpace(startingday))
+			{
+				ngayVaoLam = DateTime.Parse(startingday);
+			}
+			_viewModel.NgayVL = ngayVaoLam;
+			_viewModel.DiaChi = diachi;
+			_viewModel.BangCap = bangcap;
+
+			//Act
+			_viewModel.ValidateFullName();
+			_viewModel.ValidateSDT();
+			_viewModel.ValidateEmail();
+			_viewModel.ValidateBirth();
+			_viewModel.ValidateNgayVL();
+			_viewModel.ValidateDiaChi();
+			_viewModel.ValidateBangCap();
+
+			Assert.That(_viewModel.HoTenError, Is.EqualTo(expectedFullNameError));
+			Assert.That(_viewModel.SDTError, Is.EqualTo(expectedSDTError));
+			Assert.That(_viewModel.EmailError, Is.EqualTo(expectedEmailError));
+			Assert.That(_viewModel.NgaySinhError, Is.EqualTo(expectedNgaySinhError));
+			Assert.That(_viewModel.NgayVLError, Is.EqualTo(expectedNgayVLError));
+			Assert.That(_viewModel.DiaChiError, Is.EqualTo(expectedDiaChiError));
+			Assert.That(_viewModel.BangCapError, Is.EqualTo(expectedBangCapError));
 		}
 	}
 }
