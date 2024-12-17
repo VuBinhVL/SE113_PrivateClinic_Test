296bef534bdbfd8f6e58560d2c233372ca2e16b5
Dung Tien The <dungtienthe1920@gmail.com>
2024-12-17 22:28:10 +0700

    Add Base AutomationTest

---
+++ AutomationTest/Helpers/WindowHelper.cs
@@ -0,0 +1,26 @@
+﻿using FlaUI.Core.AutomationElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTest.Helpers
+{
+    public static class WindowHelper
+    {
+        public static Window SearchWindowByTitle(Window[] windows, string title)
+        {
+            Window result = null;
+            foreach (var w in windows)
+            {
+                if (w.Name == title)
+                {
+                    result = w;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
